0a0a0931f362495f4d7fcfa935bf8f7b8efdcc9c
emirojosue <emiroceballos@gmail.com>
2026-03-16 19:49:47 -0500

    preimer commit

---
+++ inventario.py/.py
@@ -0,0 +1,11 @@
+nombre= "strig"
+precio=float
+cantidad=int
+
+nombre= input("ingrese el nombre del producto: " )
+precio= float(input("ingrese el precio: "))
+cantidad= int(input("ingrese la cantidad: "))
+total= precio*cantidad
+print("producto:", nombre)
+print("total:", total)
+
